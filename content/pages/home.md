a7bf1e9b6bf26eabd48d8144f018915911ca322b
Anand Shekhawat <anandsingh215@yahoo.com>
2020-12-04 04:28:21 +0000

    update

--- content/pages/home.md
+++ content/pages/home.md
@@ -1,8 +1,9 @@
-title: Home
-date: Dec-03-2020
-
-![So Schmexy][miui_photo]
-
-Welcome To Whyred Miui ports
-
+title: HOME
+date: 2020-11-25
+modified: 2020-11-25
+save_as: index.html
 [miui_photo]: {static}/images/miui.jpg
+![So Schmexy][miui_photo]
+##Welcome
+[EU](eu)
+[MMX](mmx)
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Anand Shekhawat'
 SITENAME = 'Whyred Miui Ports'
 SITEURL = 'https://whyredmiuiports.github.io'
 
@@ -9,14 +8,17 @@ PATH = 'content'
 
 TIMEZONE = 'Asia/Kolkata'
 
+SLUGIFY_SOURCE = 'basename'
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 5
+SUMMARY_MAX_LENGTH = 20
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+#FEED_ALL_ATOM = None
+#CATEGORY_FEED_ATOM = None
+#TRANSLATION_FEED_ATOM = None
+#AUTHOR_FEED_ATOM = None
+#AUTHOR_FEED_RSS = None
 
 # Blogroll
 #LINKS = (('Pelican', 'https://getpelican.com/'),
@@ -28,20 +30,23 @@ AUTHOR_FEED_RSS = None
 #SOCIAL = (('You can add links in your config file', '#'),
 #          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 1
-
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY= True
 IGNORE_FILES = ['__pycache__']
 
 USE_FOLDER_AS_CATEGORY = False
-DEFAULT_CATEGORY = 'Home'
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
 
 MENUITEMS = (
-  ('EU','/eu/'),
-  ('MMX','/mmx/'),
+  ('Home','/'),
+  ('EU','/eu'),
+  ('MMX','/mmx'),
 )
+
+PATH_METADATA = '(?P<path_no_ext>.*)\..*'
+ARTICLE_URL = ARTICLE_SAVE_AS = PAGE_URL = PAGE_SAVE_AS = '{path_no_ext}.html'
+#CATEGORY_URL = '{slug}.html'
+#CATEGORY_SAVE_AS = '{slug}.html'
